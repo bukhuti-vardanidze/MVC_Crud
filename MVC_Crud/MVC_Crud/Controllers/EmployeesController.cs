215357038fe8bbc2f3ca15b1aa43ed2f98628741
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-05-29 19:53:58 +0400

    Add  /Add Employe/ Method

---
+++ MVC_Crud/MVC_Crud/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Crud.Data;
+using MVC_Crud.Models;
+using MVC_Crud.Models.Domain;
+
+namespace MVC_Crud.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MvcDbContext _context;
+
+        public EmployeesController(MvcDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBith = addEmployeeRequest.DateOfBith,
+            };
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ MVC_Crud/MVC_Crud/Models/AddEmployeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_Crud.Models
+{
+    public class AddEmployeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateOfBith { get; set; }
+        public string Department { get; set; }
+    }
+}
