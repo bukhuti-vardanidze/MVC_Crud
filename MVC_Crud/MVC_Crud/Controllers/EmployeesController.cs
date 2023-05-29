238e86b1177bd11402daa4c210f1464e1d68d995
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-05-29 21:22:53 +0400

    Add Update Employee

--- MVC_Crud/MVC_Crud/Controllers/EmployeesController.cs
+++ MVC_Crud/MVC_Crud/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC_Crud.Data;
 using MVC_Crud.Models;
 using MVC_Crud.Models.Domain;
@@ -13,6 +14,15 @@ namespace MVC_Crud.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+          var employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+
+
         [HttpGet]
         public IActionResult Add()
         {
@@ -29,12 +39,57 @@ namespace MVC_Crud.Controllers
                 Email = addEmployeeRequest.Email,
                 Salary = addEmployeeRequest.Salary,
                 Department = addEmployeeRequest.Department,
-                DateOfBith = addEmployeeRequest.DateOfBith,
+                DateOfBith = addEmployeeRequest.DateOfBith
             };
             await _context.Employees.AddAsync(employee);
             await _context.SaveChangesAsync();
 
-            return RedirectToAction("Add");
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await _context.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(employee != null)
+            {
+                var viewModel = new UpdateEmployeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBith = employee.DateOfBith
+                };
+
+                return await Task.Run(() => View("View",viewModel));
+
+            }
+      
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeViewModel viewModel)
+        {
+            var employee = await _context.Employees.FindAsync(viewModel.Id);
+          
+            if(employee != null)
+            {
+                employee.Name= viewModel.Name;
+                employee.Email= viewModel.Email;
+                employee.Salary= viewModel.Salary;
+                employee.Department= viewModel.Department;
+                employee.DateOfBith= viewModel.DateOfBith;
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
---
+++ MVC_Crud/MVC_Crud/Models/UpdateEmployeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC_Crud.Models
+{
+	public class UpdateEmployeViewModel
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public double Salary { get; set; }
+		public DateTime DateOfBith { get; set; }
+		public string Department { get; set; }
+	}
+}
